2dd191e94311b4867db6d8728522b9e9c87af606
Simon Cruanes <simon.cruanes.2007@m4x.org>
2018-08-24 19:05:37 -0500

    test: update test script to gather basic statistics

--- benchs/test.py
+++ benchs/test.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 
-import os, subprocess
+import os, subprocess, dataclasses, time
+
+timeout=os.getenv('TIMEOUT','30')
+print(f'timeout: {timeout}')
 solvers = {
-        'minisat': ['-cpu-lim=30'],
-        './ratsat-bin': ['--cpu-lim', '30'],
+        'minisat': [f'-cpu-lim={timeout}'],
+        './ratsat-bin': ['--cpu-lim', timeout],
         }
+
+@dataclasses.dataclass
+class Stats(object):
+    unknown: int = 0
+    sat: int = 0
+    unsat: int = 0
+    time: float = 0.
 
 def test_dir(d):
     print(f"test {d}")
     failures = []
+    stats = {name: Stats() for name, _ in solvers.items()}
     for (root, _, files) in os.walk(d):
         for file in files:
             file = os.path.join(root,file)
@@ -16,22 +27,25 @@ def test_dir(d):
             per_solver = {}
             expect = None
             for s, args in solvers.items():
+                start = time.time()
                 code = subprocess.call([s] + args + [file], stdout=subprocess.DEVNULL)
-                if code == 0: res = 'UNKNOWN'
-                if code == 10: res = 'SAT'
-                if code == 20: res = 'UNSAT'
+                time = time.time() - start
+                if code == 0: res = 'UNKNOWN'; stats[s].unknown += 1
+                if code == 10: res = 'SAT'; stats[s].sat += 1; stats[s].time += time
+                if code == 20: res = 'UNSAT'; stats[s].unsat += 1; stats[s].time += time
                 if expect is not None:
-                    if expect != res:
+                    if expect != res and res != 'UNKNOWN':
                         print(f"failure on {file}: {per_solver}")
                         failures.append((file, per_solver))
                 else:
                     expect = res
                 per_solver[s] = res
             print(f"on {file}: {per_solver}")
-    return failures
+    return stats, failures
 
-fails = test_dir("benchs/basic")
+stats, fails = test_dir("benchs/basic")
 
+print(stats)
 if fails:
     print("SOME FAILURES")
     print(fails)
