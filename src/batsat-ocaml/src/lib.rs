43860f2e1389405835e87a1f99c3e1357c7d77e1
Simon Cruanes <simon.cruanes.2007@m4x.org>
2018-09-02 23:37:29 -0500

    refactor(ocaml): use a macro to safely borrow solver

--- src/batsat-ocaml/src/lib.rs
+++ src/batsat-ocaml/src/lib.rs
@@ -6,6 +6,7 @@ extern crate ocaml;
 
 use ocaml::{Value,value};
 
+use std::ptr;
 use std::mem;
 use std::ops;
 
@@ -57,17 +58,29 @@ impl ops::DerefMut for Solver {
     fn deref_mut(&mut self) -> &mut Self::Target { &mut self.s }
 }
 
-#[inline]
-fn get_solver(v: Value) -> Box<Solver> {
-    let ptr : *mut Solver = v.custom_ptr_val_mut();
-    unsafe{ Box::from_raw(ptr as *mut Solver) }
+// macro to locally borrow solver. `with_solver!(s, v, block)`
+// runs `block` in a context where `s` binds to a `&mut solver` from `v`
+macro_rules! with_solver {
+    ($s: ident, $v:expr, $code:block) => {
+        {
+            let $s = &mut *($v.custom_ptr_val_mut() as *mut Solver) as &mut Solver;
+            $code;
+        };
+    }
+}
+
+fn delete_value(v: Value) {
+    if (v.custom_ptr_val() as *const Solver) != ptr::null() {
+        let s = unsafe { Box::from_raw(v.custom_ptr_val_mut() as *mut Solver) };
+        mem::drop(s); // delete!
+    }
+    // be sure not to delete twice
+    unsafe { * v.custom_ptr_val_mut() = ptr::null() as *const Solver };
 }
 
 // finalizer for values
 extern "C" fn batsat_finalizer(v: ocaml::core::Value) {
-    let v = Value::new(v);
-    let s = get_solver(v);
-    mem::drop(s); // delete!
+    delete_value(Value::new(v));
 }
 
 caml!(ml_batsat_new, |_params|, <res>, {
@@ -77,130 +90,122 @@ caml!(ml_batsat_new, |_params|, <res>, {
 } -> res);
 
 caml!(ml_batsat_delete, |param|, <res>, {
-    // FIXME:
-    // already cleaned?
-    // if (*((solver**)(Data_custom_val(block)))==0) {
-    //     goto exit;
-    // }
-    let s = get_solver(param);
-    mem::drop(s);
+    delete_value(param);
     res = value::UNIT;
 } -> res);
 
 caml!(ml_batsat_simplify, |ptr|, <res>, {
-    let mut solver = get_solver(ptr);
-    let r = solver.simplify().into();
-    mem::forget(solver);
-    res = Value::bool(r);
+    with_solver!(solver, ptr, {
+        let r = solver.simplify().into();
+        res = Value::bool(r);
+    })
 } -> res);
 
 /// Add literal, or add clause if the lit is 0
 caml!(ml_batsat_addlit, |ptr, lit|, <res>, {
-    let mut solver = get_solver(ptr);
-    let lit = lit.i32_val();
+    with_solver!(solver, ptr, {
+        let lit = lit.i32_val();
 
-    let mut r = true;
-    if lit == 0 {
-        // push current clause into vector `clauses`, reset it
-        //println!("add-lit {:?}", 0);
-        let (solver, cur, _) = solver.decompose();
-        r = solver.add_clause_reuse(cur);
-        cur.clear();
-    } else {
-        // push literal into clause
-        let lit = solver.get_lit(lit);
-        //println!("add-lit {:?}", lit);
-        solver.cur_clause.push(lit);
-    }
-
-    mem::forget(solver);
-    res = Value::bool(r);
+        let mut r = true;
+        if lit == 0 {
+            // push current clause into vector `clauses`, reset it
+            //println!("add-lit {:?}", 0);
+            let (solver, cur, _) = solver.decompose();
+            r = solver.add_clause_reuse(cur);
+            cur.clear();
+        } else {
+            // push literal into clause
+            let lit = solver.get_lit(lit);
+            //println!("add-lit {:?}", lit);
+            solver.cur_clause.push(lit);
+        }
+        res = Value::bool(r);
+    })
 } -> res);
 
 /// Add assumption into the solver
 caml!(ml_batsat_assume, |ptr, lit|, <res>, {
-    let mut solver = get_solver(ptr);
-    let lit = lit.i32_val();
+    with_solver!(solver, ptr, {
+        let lit = lit.i32_val();
 
-    assert!(lit != 0);
-    let lit = solver.get_lit(lit);
-    solver.assumptions.push(lit);
+        assert!(lit != 0);
+        let lit = solver.get_lit(lit);
+        solver.assumptions.push(lit);
 
-    mem::forget(solver);
-    res = value::UNIT;
+        res = value::UNIT;
+    })
 } -> res);
 
 caml!(ml_batsat_solve, |ptr|, <res>, {
-    let mut solver = get_solver(ptr);
+    with_solver!(solver, ptr, {
+        let r = {
+            let (s, _, assumptions) = solver.decompose();
+            let lb = s.solve_limited(&assumptions);
+            assumptions.clear(); // reset assumptions
+            assert_ne!(lb, lbool::UNDEF); // can't express that in a bool
+            lb != lbool::FALSE
+        };
+        //println!("res: {:?}, model: {:?}", r, solver.get_model());
+        res = Value::bool(r);
 
-    let r = {
-        let (s, _, assumptions) = solver.decompose();
-        let lb = s.solve_limited(&assumptions);
-        assumptions.clear(); // reset assumptions
-        assert_ne!(lb, lbool::UNDEF); // can't express that in a bool
-        lb != lbool::FALSE
-    };
-    //println!("res: {:?}, model: {:?}", r, solver.get_model());
-    mem::forget(solver);
-    res = Value::bool(r);
+    })
 } -> res);
 
 caml!(ml_batsat_value, |ptr, lit|, <res>, {
-    let mut solver = get_solver(ptr);
-    let lit = lit.i32_val();
-    let r =
-        if lit.abs() >= solver.num_vars() as i32 {
-            lbool::UNDEF
-        } else {
-            let lit = solver.get_lit(lit as i32);
-            solver.s.value_lit(lit)
-        };
-    //println!("val for {:?}: {:?}", lit, r);
-    mem::forget(solver);
-    res = Value::i32(r.to_u8() as i32);
+    with_solver!(solver, ptr, {
+        let lit = lit.i32_val();
+        let r =
+            if lit.abs() >= solver.num_vars() as i32 {
+                lbool::UNDEF
+            } else {
+                let lit = solver.get_lit(lit as i32);
+                solver.s.value_lit(lit)
+            };
+        //println!("val for {:?}: {:?}", lit, r);
+        res = Value::i32(r.to_u8() as i32);
+
+    });
 } -> res);
 
 caml!(ml_batsat_check_assumption, |ptr, lit|, <res>, {
-    let mut solver = get_solver(ptr);
-    let lit = lit.i32_val();
+    with_solver!(solver, ptr, {
+        let lit = lit.i32_val();
 
-    // check unsat-core
-    let lit = solver.get_lit(lit);
-    let r = solver.s.unsat_core_contains_var(lit.var());
+        // check unsat-core
+        let lit = solver.get_lit(lit);
+        let r = solver.s.unsat_core_contains_var(lit.var());
 
-    mem::forget(solver);
-    res = Value::bool(r);
+        res = Value::bool(r);
+    })
 } -> res);
 
 caml!(ml_batsat_set_verbose, |ptr, level|, <res>, {
-    let mut solver = get_solver(ptr);
-    let level = level.i32_val();
-
-    solver.s.set_verbosity(level);
-
-    mem::forget(solver);
-    res = value::UNIT;
+    with_solver!(solver, ptr, {
+        let level = level.i32_val();
+        solver.s.set_verbosity(level);
+        res = value::UNIT;
+    })
 } -> res);
 
 caml!(ml_batsat_nvars, |ptr|, <res>, {
-    let solver = get_solver(ptr);
-    let r = solver.s.num_vars() as i64;
-    mem::forget(solver);
-    res = Value::i64(r);
+    with_solver!(solver, ptr, {
+        let r = solver.s.num_vars() as i64;
+        res = Value::i64(r);
+    });
 } -> res);
 
 caml!(ml_batsat_nclauses, |ptr|, <res>, {
-    let solver = get_solver(ptr);
-    let r = solver.s.num_clauses();
-    mem::forget(solver);
-    res = Value::i64(r as i64);
+    with_solver!(solver, ptr, {
+        let r = solver.s.num_clauses();
+        res = Value::i64(r as i64);
+    })
 } -> res);
 
 caml!(ml_batsat_nconflicts, |ptr|, <res>, {
-    let solver = get_solver(ptr);
-    let r = solver.s.num_conflicts();
-    mem::forget(solver);
-    res = Value::i64(r as i64);
+    with_solver!(solver, ptr, {
+        let r = solver.s.num_conflicts();
+        res = Value::i64(r as i64);
+    })
 } -> res);
 
 
